c16245d221de125fadc40b44ff9f1fbb166db21e
Satyam tiwari <140236808+STym108@users.noreply.github.com>
2026-07-24 22:39:45 +0530

    Time: 430 ms (34.26%), Space: 211.7 MB (35.31%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-int explore(vector<int>&prices,int idx,int state,int limit,vector<vector<vector<int>>>&dp){
-    if(idx>=prices.size()||limit<=0) return 0;
-    if(dp[idx][state][limit]!=-1) return dp[idx][state][limit];
-    int take=0;
-    int skip=0;  //not bought state=-1, bought state=1,sold state=0;
-    if(state==0){
-      take=-prices[idx]+explore(prices,idx+1,1,limit,dp);
-    }
-    if(state==1){
-        take=prices[idx]+explore(prices,idx+1,0,limit-1,dp);
-    }
-    skip=explore(prices,idx+1,state,limit,dp);
-    return dp[idx][state][limit]= max(take,skip);
-}
+
     int maxProfit(vector<int>& prices) {
     // vector<vector<int>>(2,vector<int>(2,-1))
-    vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(3,vector<int>(3,-1)));
-    return explore(prices,0,0,2,dp);
+    vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(2,vector<int>(3,0)));
+    int n=prices.size();
+vector<vector<int>>after(2,vector<int>(3,0));
+vector<vector<int>>curr(2,vector<int>(3,0));
+    for(int i=n-1;i>=0;i--){
+    for(int buy=0;buy<=1;buy++){
+    for(int limit=2;limit>=1;limit--){
+        if(buy==1){
+            curr[buy][limit]= max(-prices[i]+after[0][limit],after[buy][limit]);
+        }
+        else if(buy==0){
+            curr[buy][limit]=max(prices[i]+after[1][limit-1],after[buy][limit]);
+        }
+    }
+    }
+    after=curr;
+    }
+    return curr[1][2];
     }
 };
