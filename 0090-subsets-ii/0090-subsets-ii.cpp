7b946add55d50256339b0de2099dde59d778ef24
Satyam tiwari <140236808+STym108@users.noreply.github.com>
2026-07-30 19:31:20 +0530

    Time: 1 ms (45.26%), Space: 10.8 MB (40.63%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void getans(vector<int>& nums,int idx,vector<int>temp,vector<vector<int>>&ans){
+
+    for(int i=idx;i<nums.size();i++){
+       if(i>idx&&nums[i]==nums[i-1]) continue;
+      else  {temp.push_back(nums[i]);
+        getans(nums,i+1,temp,ans);
+        temp.pop_back();}
+    }
+    ans.push_back(temp);
+    return ;
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    sort(nums.begin(),nums.end());
+    int n=nums.size();
+    vector<vector<int>>ans;
+   vector<int>temp={};
+    getans(nums,0,temp,ans);
+
+    return ans;
+    }
+};
