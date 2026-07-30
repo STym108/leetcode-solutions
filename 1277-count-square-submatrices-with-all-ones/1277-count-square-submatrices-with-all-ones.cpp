9348cc02d41b9cd338e39997040696dd03aec80b
Satyam tiwari <140236808+STym108@users.noreply.github.com>
2026-07-30 10:03:31 +0530

    Time: 24 ms (7.47%), Space: 31.6 MB (5.34%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -3,9 +3,9 @@ public:
 int explore(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&dp){
     if(i>=grid.size()||i<0||j>=grid[0].size()||j<0||grid[i][j]==0) return 0;
     if(dp[i][j]!=-1) return dp[i][j];
-    int r=explore(grid,i,j+1,dp);
-    int d=explore(grid,i+1,j+1,dp);
-    int b=explore(grid,i+1,j,dp);
+    int r=explore(grid,i,j-1,dp);
+    int d=explore(grid,i-1,j-1,dp);
+    int b=explore(grid,i-1,j,dp);
     return dp[i][j]= 1+min({r,d,b});
 }
 
@@ -14,8 +14,8 @@ int explore(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&dp){
     int c=grid[0].size();
     int ans=0;
     vector<vector<int>>dp(r,vector<int>(c,-1));
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
+    for(int i=r-1;i>=0;i--){
+        for(int j=c-1;j>=0;j--){
         if(grid[i][j]==1){
     int x= explore(grid,i,j,dp);
     ans+=x;
