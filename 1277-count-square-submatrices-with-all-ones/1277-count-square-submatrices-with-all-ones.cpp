d8008b78957928f1b7d69785cba2288c50574f32
Satyam tiwari <140236808+STym108@users.noreply.github.com>
2026-07-30 10:39:27 +0530

    Time: 3 ms (84.8%), Space: 30.3 MB (68.79%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -12,17 +12,21 @@ int explore(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&dp){
     int countSquares(vector<vector<int>>& grid) {
     int r=grid.size();
     int c=grid[0].size();
-    int ans=0;
-    vector<vector<int>>dp(r,vector<int>(c,-1));
-    for(int i=r-1;i>=0;i--){
-        for(int j=c-1;j>=0;j--){
-        if(grid[i][j]==1){
-    int x= explore(grid,i,j,dp);
-    ans+=x;
+
+    vector<vector<int>>dp(r+1,vector<int>(c+1,0));
+
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=c;j++){
+       if(grid[i-1][j-1]==1){
+        dp[i][j]=1+min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
+       }
         }
         }
+    
+     int ans=0;
+    for(int i=0;i<=r;i++){
+        for(int j=0;j<=c;j++) ans+=dp[i][j];
     }
     return ans;
-
     }
 };
