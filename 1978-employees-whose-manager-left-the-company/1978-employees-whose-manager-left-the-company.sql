e7e96f05a51f93871c6c743e0726e85a04330a23
Satyam tiwari <140236808+STym108@users.noreply.github.com>
2026-08-03 00:00:54 +0530

    Time: 419 ms (16.11%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select e1.employee_id as employee_id
+from Employees as e1
+left join Employees as e2 
+on e1.manager_id=e2.employee_id
+where e1.manager_id is not null and e1.salary<30000
+and e2.employee_id is null
+group by  e1.employee_id
+order by e1.employee_id
