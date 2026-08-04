da84badf5984c04fb159bf185e6bbd992a150dc0
Satyam tiwari <140236808+STym108@users.noreply.github.com>
2026-08-05 01:29:33 +0530

    Time: 31 ms (53.17%), Space: 27 MB (64.99%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-typedef long long int ll;
-int getans(string &str,int i,string &t,int j){
-    if(j>=t.size()) return 1;
-    else if(i>=str.size()) return 0;
-    int same=0;
-    if(str[i]==t[j]) same=getans(str,i+1,t,j+1);
-    int skip=getans(str,i+1,t,j);
-    return same+skip;
+
+int getans(string &s,int i,string&t,int j, vector<vector<int>>&dp){
+    if(j<0) return 1;
+    if(i<0) return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
+    int take=0;
+
+    if(s[i]==t[j]) take= getans(s,i-1,t,j-1,dp);
+   int skip= getans(s,i-1,t,j,dp);
+   return dp[i][j]= take+skip;
 }
+
     int numDistinct(string s, string t) {
-    // return getans(s,0,t,0);
-    int r=s.size();
-    int c=t.size();
-
-    vector<ll>prev(c+1,0);
-    prev[0]=1;
-    for(int i=1;i<r+1;i++){
-        vector<ll>curr(c+1,0);
-        for(int j=0;j<c+1;j++){
-        int x=0;
-       if(j!=0) {if(s[i-1]==t[j-1]) x=prev[j-1];}
-        curr[j]=prev[j]+x;
-        }
-        prev=curr;
-    }
-
-    return prev[c];
+    if(s.size()<t.size()) return 0;
+    vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
+    
+    return getans(s,s.size()-1,t,t.size()-1,dp);
 
     }
 };
